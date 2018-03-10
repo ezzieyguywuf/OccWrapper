b3b8fb7d8c8994fa96ff061af8d251068cad3bee
Wolfgang E. Sanyer <WolfgangESanyer@gmail.com>
2018-03-10 00:46:07 -0500

    Added OccPrimitive and made OccBox a derived of it.

---
+++ include/OccPrimitive.h
@@ -0,0 +1,35 @@
+#ifndef OCCPRIMITIVE_H
+#define OCCPRIMITIVE_H
+
+#include <OccSolid.h>
+#include <OccFace.h>
+#include <OccEdge.h>
+#include <OccTypes.h>
+
+#include <map>
+
+#include <TopoDS_Solid.hxx>
+
+namespace Occ{
+    class Primitive : public Occ::Solid
+    {
+        public:
+            inline Primitive(const TopoDS_Solid aSolid);
+
+            inline const Face& getFace(const FaceName& which) const;
+
+        protected:
+            std::map<FaceName, Face> myNamedFaces;
+    };
+
+    Primitive::Primitive(TopoDS_Solid aSolid)
+        : Solid(aSolid)
+    {}
+
+    const Face& Primitive::getFace(const FaceName& which) const
+    {
+        return myNamedFaces.at(which);
+    }
+}
+
+#endif /* OCCPRIMITIVE_H */
