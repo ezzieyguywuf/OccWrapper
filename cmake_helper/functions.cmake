f00f06aac718dbd95369db42adfc9f521fd4e0af
Wolfgang E. Sanyer <WolfgangESanyer@gmail.com>
2018-03-19 11:58:22 -0400

    Added AddLib CMake function.

--- cmake_helper/functions.cmake
+++ cmake_helper/functions.cmake
@@ -4,3 +4,7 @@ function(AddTest SrcName)
     add_test(NAME "TEST_${SrcName}" COMMAND $<TARGET_FILE:${SrcName}>)
 endfunction(AddTest)
 
+function(AddLib SrcName)
+    add_library(${SrcName} SHARED "${SrcName}.cpp")
+    target_link_libraries(${SrcName} ${ARGN})
+endfunction(AddLib)
