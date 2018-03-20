d7831036d5cd05b3f14c8e70f6f3bd0ecf6c76e1
Wolfgang E. Sanyer <WolfgangESanyer@gmail.com>
2018-03-20 06:52:28 -0400

    Simple test to explore error in makeBoolean for BRepAlgoAPI_Cut

--- test/SimpleTest.cpp
+++ test/SimpleTest.cpp
@@ -5,21 +5,46 @@
 #include <OccBooleanSolid.h>
 #include <OccTypes.h>
 
+#include <TopTools_ListOfShape.hxx>
+#include <TopTools_ListIteratorOfListOfShape.hxx>
+#include <TopoDS_Face.hxx>
+#include <TopoDS.hxx>
+#include <BRepAlgoAPI_Cut.hxx>
+#include <BinTools.hxx>
+
 #include <iostream>
 
 int main(void)
 {
     Occ::Box box = Occ::SolidMaker::makeBox(10, 10, 10);
     Occ::Cylinder cyl = Occ::SolidMaker::makeCylinder(2.5, 10);
-    Occ::BooleanSolid fused = Occ::SolidModifier::makeFusion(box, cyl);
+    BRepAlgoAPI_Cut mkCut(box.getShape(), cyl.getShape());
+    Occ::Solid newSolid(mkCut.Shape());
 
-    const Occ::Face& top1 = fused.getFaces()[3];
-    const Occ::Face& top2 = fused.getFaces()[7];
+    int i = 0;
+    for (const Occ::Face& aFace : cyl.getFaces())
+    {
+        TopTools_ListOfShape modified = mkCut.Modified(aFace.getShape());
+        TopTools_ListIteratorOfListOfShape iterator(modified);
 
-    auto v1 = fused.getConstituentFace(top1);
-    auto v2 = fused.getConstituentFace(top2);
+        for(; iterator.More(); iterator.Next())
+        {
+            TopoDS_Face modifiedFace = TopoDS::Face(iterator.Value());
+            std::cout << "face " << i << " in cyl had modified faces!"<< std::endl;
+            std::cout << std::boolalpha;
+            std::cout << "aFace == modifiedFace -> " << (aFace.getShape() == modifiedFace) << std::endl;
+            BinTools::Write(modifiedFace, "modifiedCylSurface.brep");
+            for (const Occ::Face& newFace : newSolid.getFaces())
+            {
+                if (aFace == newFace)
+                {
+                    std::cout << "Found aFace in newSolid!" << std::endl;
+                }
+            }
+        }
 
-    std::cout << v1[0] << v2[0] << std::endl;
+        i++;
+    }
 
     return 0;
 }
