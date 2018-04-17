d44eba871fe2d379bd88daa8bce496e5cb1f9867
Wolfgang E. Sanyer <WolfgangESanyer@gmail.com>
2018-04-16 23:07:56 -0400

    Added Occ::Primitive to python class hierarchy.

--- srcPy/OccSolidMakerPy.cpp
+++ srcPy/OccSolidMakerPy.cpp
@@ -5,7 +5,15 @@ namespace py = pybind11;
 
 void init_OccSolidMaker(py::module& m)
 {
-    py::class_<Occ::Box>(m, "Box");
+    py::class_<Occ::Box, Occ::Primitive>(m, "Box")
+        .doc() = "A Box. As such, it has six faces. The OccWrapper.FaceNames "
+                 "enum can be\nused to access the following faces: top, bottom, "
+                 "left, right, front, back.";
+    py::class_<Occ::Cylinder, Occ::Primitive>(m, "Cylinder")
+        .doc() = "A Cylinder. As such, it has three faces. The "
+        "OccWrapper.FaceNames enum can be\nused to access the "
+        "following faces: top, bottom, left, right, front, back.";
     py::class_<Occ::SolidMaker>(m, "SolidMaker")
-        .def("makeBox", &Occ::SolidMaker::makeBox, "Make a box");
+        .def("makeBox", &Occ::SolidMaker::makeBox, "Make a box")
+        .def("makeCylinder", &Occ::SolidMaker::makeCylinder, "Make a cylinder");
 }
--- srcPy/OccWrapperPy.cpp
+++ srcPy/OccWrapperPy.cpp
@@ -4,11 +4,16 @@
 namespace py = pybind11;
 
 void init_OccShape(py::module&);
+void init_OccPrimitive(py::module&);
+void init_OccEdge(py::module&);
 void init_OccSolidMaker(py::module&);
 
 PYBIND11_MODULE(OccWrapper, m)
 {
     m.doc() = "A wrapper to open-cascade, available in Python.";
     init_OccShape(m);
+    init_OccEdge(m);
+    init_OccPrimitive(m);
+    //init_OccBox(m);
     init_OccSolidMaker(m);
 }
