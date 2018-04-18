74c24cdf8e4ad9a837fce01ae2d63bc4c94f0dd8
Wolfgang E. Sanyer <WolfgangESanyer@gmail.com>
2018-04-17 22:11:49 -0400

    Finished adding ModifiedSolid methods.

--- srcPy/OccModifiedSolidPy.cpp
+++ srcPy/OccModifiedSolidPy.cpp
@@ -57,6 +57,31 @@ R"mydelimiter(
             Each item in the list is an index that refers to an item in the
             `newSolid.getFaces()` list.
 )mydelimiter"))
+        .def("__eq__", [](const Occ::ModifiedSolid& s1, const Occ::ModifiedSolid& s2){
+                return s1 == s2;}, 
+             py::is_operator(),
+             py::arg("aModifiedSolid"),
+             py::doc("Tests whether self and aModifiedSolid are equal."))
+        .def("__ne__", [](const Occ::ModifiedSolid& s1, const Occ::ModifiedSolid& s2){
+                return s1 != s2;}, py::is_operator())
+        .def("getNewSolid", &Occ::ModifiedSolid::getNewSolid,
+             py::doc("returns the new solid"))
+        .def("getOrigSolid", &Occ::ModifiedSolid::getOrigSolid,
+             py::doc("returns the original solid"))
+        .def("getModifiedFaceIndices", &Occ::ModifiedSolid::getModifiedFaceIndices,
+             py::arg("aFace"),
+             py::doc(R"pbdoc(
+                Returns a vector of indices which correspond to faces in the new
+                solid. These faces represent modification(s) of `aFace`. The
+                vector will be sorted.
+             )pbdoc"))
+        .def("isDeleted", &Occ::ModifiedSolid::isDeleted,
+             py::arg("Face"),
+             py::doc("Check if `aFace` was deleted"))
+        .def("getNewFaceIndices", &Occ::ModifiedSolid::getNewFaceIndices,
+             py::doc("Return the new faces"))
+        .def("getNewFaceMap", &Occ::ModifiedSolid::getNewFaceMap,
+             py::doc("returns the new faces"))
         .doc() = R"pbdoc(
         A ModifiedSolid is a helper class used for communication. It contains
         information regarding a topological update of a solid. This information
