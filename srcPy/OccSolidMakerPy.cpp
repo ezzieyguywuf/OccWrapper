c6ca1e07fd4adc1065e502c953ad38fc72ac53a5
Wolfgang E. Sanyer <WolfgangESanyer@gmail.com>
2018-04-18 11:13:59 -0400

    Updated documentation.

--- srcPy/OccSolidMakerPy.cpp
+++ srcPy/OccSolidMakerPy.cpp
@@ -6,6 +6,7 @@ namespace py = pybind11;
 void init_OccSolidMaker(py::module& m)
 {
     py::class_<Occ::SolidMaker>(m, "SolidMaker")
-        .def("makeBox", &Occ::SolidMaker::makeBox, "Make a box")
-        .def("makeCylinder", &Occ::SolidMaker::makeCylinder, "Make a cylinder");
+        .def("makeBox", &Occ::SolidMaker::makeBox, "Make a box with one corner on the origin")
+        .def("makeCylinder", &Occ::SolidMaker::makeCylinder, "Make a cylinder centered on the origin.");
+        ;
 }
