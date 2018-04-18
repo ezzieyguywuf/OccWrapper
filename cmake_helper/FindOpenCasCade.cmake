95f9cd2bd169528c1d663f02725b9af56f535dbf
Wolfgang E. Sanyer <WolfgangESanyer@gmail.com>
2018-04-18 13:53:20 -0400

    Added FindOpenCascade and use it and the variables it produces.

---
+++ cmake_helper/FindOpenCasCade.cmake
@@ -0,0 +1,47 @@
+# Try to find OPENCASCADE
+# Once done, this will define:
+# OPENCASCADE_FOUND - System has OpenCasCade
+# OPENCASCADE_INCLUDE_DIRS - The OpenCasCade include directories
+# OPENCASCADE_LIBRARIES - The libraries needed to use OpenCasCade
+
+set (OPENCASCADE_FOUND "NO")
+
+find_path(OPENCASCADE_INCLUDE_DIR TopoDS_Shape.hxx
+    PATH_SUFFIXES opencascade)
+
+find_library(OPENCASCADE_LIBRARY 
+    NAMES TKBRep)
+
+get_filename_component(OPENCASCADE_LIBRARIES_DIR ${OPENCASCADE_LIBRARY} DIRECTORY CACHE)
+
+include(FindPackageHandleStandardArgs) # to handle standard args, provided by cMake (?)
+find_package_handle_standard_args(OPENCASCADE DEFAULT_MSG
+                                  OPENCASCADE_INCLUDE_DIR OPENCASCADE_LIBRARY)
+if (OPENCASCADE_FOUND)
+    message("-- Found OpenCasCade")
+    message("-- -- OpenCasCade include directory = ${OPENCASCADE_INCLUDE_DIR}")
+    message("-- -- OpenCasCade library directory = ${OPENCASCADE_LIBRARIES_DIR}")
+endif(OPENCASCADE_FOUND)
+
+mark_as_advanced(OPENCASCADE_INCLUDE_DIR OPENCASCADE_LIBRARY)
+set(OPENCASCADE_INCLUDE_DIRS ${OPENCASCADE_INCLUDE_DIR} CACHE PATH "OCC include directory.")
+set(OPENCASCADE_LIBRARIES 
+    TKFillet
+    TKernel
+    TKG2d
+    TKG3d
+    TKMath 
+    TKShHealing
+    TKBool
+    TKBO
+    TKBRep 
+    TKTopAlgo
+    TKGeomAlgo
+    TKGeomBase
+    TKPrim
+    TKFeat
+
+    TKCAF
+    TKLCAF
+    )
+
